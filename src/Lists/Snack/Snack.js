cdb8b3fcfdb3b89cd2b1c7de727fb7fa3637ada5
Ari Harlem-Caballero <ari.bash.harlem@gmail.com>
2022-01-28 11:07:47 -0800

    started snack.js

--- src/Lists/Snack/Snack.js
+++ src/Lists/Snack/Snack.js
@@ -1,5 +1,17 @@
 import React from 'react';
 
-export default function Snack() {
-  return <div></div>;
+export default function Snack({
+  name,
+  type,
+  img,
+  ingredients
+}) {
+  return <div className='snack-div'>
+    <p>Name: {name}</p>
+    <p>Type: {type}</p>
+    <img alt={name} src={img}/>
+    <div className='ingredients-div'>
+      
+    </div>
+  </div>;
 }
