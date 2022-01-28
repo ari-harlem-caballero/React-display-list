d9cc72ef753d7452ac6890616e2fe8933c2f8174
Ari Harlem-Caballero <ari.bash.harlem@gmail.com>
2022-01-28 10:09:59 -0800

    import img for music-data, wrote Music.js function

--- src/Lists/Music/Music.js
+++ src/Lists/Music/Music.js
@@ -1,6 +1,20 @@
 import React from 'react';
 
-export default function Music() {
-  return <div></div>;
+export default function Music({
+  artist,
+  type,
+  img,
+  album
+}) {
+  return <div className='music-div'>
+    <p>Artist: {artist}</p>
+    <p>Type: {type}</p>
+    <img alt={artist} src={img}/>
+    <div className='music-album'>
+      <p>Album Title: {album.title}</p>
+      <p>Release Date: {album.date}</p>
+      <img alt={album.title} src={album.img}/>
+    </div>
+  </div>;
 }
 
